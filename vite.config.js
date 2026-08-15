cf694ee611eaac927c81715796896caa6a9ba457
Joseph P. <joseph.pena.q@gmail.com>
2026-08-14 23:41:35 -0300

    build: Add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,8 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  // Project Pages URL: https://joseph-pq.github.io/yugioh-simulator/
+  base: process.env.NODE_ENV === 'production' ? '/yugioh-simulator/' : '/',
   plugins: [
     react(),
     tailwindcss(),
